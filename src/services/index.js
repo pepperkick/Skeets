ca7e44c271c8ab23324cf59d057bcf5efab21dca
PepperKick <abhishrant.m@gmail.com>
2017-10-27 09:20:22 +0530

    added action registry

--- src/services/index.js
+++ src/services/index.js
@@ -3,7 +3,12 @@ import messages from './messages';
 import natural from './natural';
 import dialogflow from './dialogflow';
 
+const actionRegistry = {};
+
 export default async (app) => {
+    app.registerAction = (action, func) => actionRegistry[action] = func;
+    app.callAction = (action, data) => actionRegistry[action](data);
+
     return {
         discord: await discord(app),
         messages: await messages(app),
